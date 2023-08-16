9d5c3c75c7c25161a47f50bfa1f5cfc35a19bc86
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-16 08:21:03 +0300

    fix: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,23 @@
 import { Router } from 'express';
 
-import authRoute from './auth';
-import userRoute from './user';
-import passRoute from './password';
-import passwordAuth from './passwordAuth';
-import authMiddleware from '../middlewares/auth';
+import authRoute from './auth-route';
+import userRoute from './user-route';
+import passRoute from './password-route';
+import passwordAuth from './password-auth-route';
+import projectRoute from './project-route';
+import bookRoute from './book-route';
+import blockRoute from './block-route';
+
+import authMiddleware from '../middlewares/auth-middleware';
 
 const router = Router();
 
 router.use('/', authRoute);
 router.use('/', passwordAuth);
 router.use('/', authMiddleware, userRoute);
+router.use('/', authMiddleware, projectRoute);
+router.use('/', authMiddleware, bookRoute);
+router.use('/', authMiddleware, blockRoute);
 router.use('/', authMiddleware, passRoute);
 
 export default router;
