0d51c83ae6064e77b5a1bf70c2bcc58710e13474
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-16 08:16:21 +0300

    fix: project-model

---
+++ src/models/project-model.ts
@@ -0,0 +1,35 @@
+import {
+  Schema, Document, model, Model,
+} from 'mongoose';
+
+export interface IProject extends Document {
+  value: string;
+  label: string;
+  owner: Schema.Types.ObjectId;
+}
+
+export interface ProjectModel extends Model<IProject> {
+  findProjectByUser: (id: string) => Promise<IProject | undefined>;
+}
+
+const ProjectSchema = new Schema({
+  value: {
+    type: String,
+    required: true,
+    minlength: 2,
+    // maxlength: 30,
+  },
+  label: {
+    type: String,
+    required: true,
+    minlength: 2,
+    // maxlength: 30,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
+
+export default model<IProject, ProjectModel>('Project', ProjectSchema);
