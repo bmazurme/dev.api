6f9a3a1b9412a74ec817c76ff32998cb8a6601ad
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-16 08:17:00 +0300

    fix: book-model

---
+++ src/models/book-model.ts
@@ -0,0 +1,36 @@
+import {
+  Schema, Document, model, Model,
+} from 'mongoose';
+
+export interface IBook extends Document {
+  name: string;
+  typeBook: string;
+  projectId: Schema.Types.ObjectId;
+  // userId: Schema.Types.ObjectId;
+}
+
+export interface BookModel extends Model<IBook> {
+  findBookById: (id: string) => Promise<IBook | undefined>;
+}
+
+const BookSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    // maxlength: 30,
+  },
+  typeBook: {
+    type: String,
+    required: true,
+    minlength: 2,
+    // maxlength: 30,
+  },
+  projectId: {
+    type: Schema.Types.ObjectId,
+    ref: 'project',
+    required: true,
+  },
+});
+
+export default model<IBook, BookModel>('Book', BookSchema);
